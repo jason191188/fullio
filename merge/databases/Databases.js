2c068d1860dd57642c98bf9132a176487d6a356a
jason191188 <mytreasure1164@gmail.com>
2023-02-23 15:26:06 +0900

    members

--- merge/databases/Databases.js
+++ merge/databases/Databases.js
@@ -2,31 +2,31 @@ module.exports = [
   {
     id : 1,
     user : '22207003',
-    pw : '1111',
+    pw : '4184',
     username : '강지용' 
   },
   {
     id : 2,
     user : '22207005',
-    pw : '2222',
+    pw : '6407',
     username : '고기훈' 
   },
   {
     id : 3,
     user : '22207007',
-    pw : '3333',
+    pw : '9679',
     username : '고재영' 
   },
   {
     id : 4,
     user : '22207045',
-    pw : '4444',
+    pw : '7662',
     username : '오주연' 
   },
   {
     id : 5,
-    user : 'hyeon',
-    pw : '5555',
+    user : '22207072',
+    pw : '9050',
     username : '진승현' 
   }
 ]

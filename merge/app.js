50bc33d14ffa90411e7412eb8a21d87e370da3b6
jason191188 <mytreasure1164@gmail.com>
2023-03-07 15:56:03 +0900

    cors

--- merge/app.js
+++ merge/app.js
@@ -26,7 +26,7 @@ dotenv.config();
 app.use(express.json());
 app.use(cookieparser());
 app.use(cors({
-  origin : ["http://3.37.94.31", /\.fullio\.kr$/], 
+  origin : "http://fullio.kr",  
   methods : ['GET', 'POST', 'PUT', 'DELETE'],
   credentials : true,
 }));

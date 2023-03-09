284966727f7bd2099afdeeb7e90cf697f324683a
강지용 <yong@gangjiyong-ui-MacBookPro.local>
2023-03-09 13:35:25 +0900

    member 테이블 UPDATE

--- merge/databases/Databases.js
+++ merge/databases/Databases.js
@@ -15,3 +15,54 @@ exports.select = ( memberNumber, password ) => {
     });
   })
 };
+
+//member 테이블에서 nickName, phoneNumber, email, motto, profil_path 값이 들어올 경우 UPDATE 실행
+exports.updateMember = (memberNumber, updateData) => {
+  return new Promise((resolve, reject) => {
+    const query = 'SELECT * FROM member WHERE memberNumber = ? LIMIT 1';
+    cnn.query(query, [memberNumber], (err, data) => {
+      if (err) {
+        reject(err);
+      } else if (data.length === 0) {
+        reject('Member not found');
+      } else {
+        let updateFields = [];
+        let updateValues = [];
+        if (updateData.nickName) {
+          updateFields.push('nickName = ?');
+          updateValues.push(updateData.nickName);
+        }
+        if (updateData.phoneNumber) {
+          updateFields.push('phoneNumber = ?');
+          updateValues.push(updateData.phoneNumber);
+        }
+        if (updateData.email) {
+          updateFields.push('email = ?');
+          updateValues.push(updateData.email);
+        }
+        if (updateData.motto) {
+          updateFields.push('motto = ?');
+          updateValues.push(updateData.motto);
+        }
+        if (updateData.profil_path) {
+          updateFields.push('profil_path = ?');
+          updateValues.push(updateData.profil_path);
+        }
+        if (updateFields.length === 0) {
+          reject('No update fields provided');
+        } else {
+          updateValues.push(memberNumber);
+          const query = `UPDATE member SET ${updateFields.join(', ')} WHERE memberNumber = ?`;
+          cnn.query(query, updateValues, (err, result) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(`Member ${memberNumber} updated successfully`);
+            }
+          });
+        }
+      }
+    });
+  });
+};
+
---
+++ merge/databases/ex.js
@@ -0,0 +1,35 @@
+
+// member 테이블 UPDATE
+// const test = require('./database.js');
+
+// const memberNumber = 22207003 
+// const updatedData = {
+//     nickName : "강지용333",
+//     phoneNumber : "010-1111-2222",
+//     email : "test@naver.com", 
+//     motto : "하리보333",
+//     profil_path : "/Users/yong/Desktop/MERGE/팀원미모티콘/지용님.png"
+// };
+
+
+// test.updateMember(memberNumber, updatedData).then(function(data) {
+//     console.log(data);
+// });
+
+
+// skill 테이블 INSERT 
+// const test = require('./database.js');
+
+// const memberNumber = 22207003 
+// const updatedData = {
+//     nickName : "강지용333",
+//     phoneNumber : "010-1111-2222",
+//     email : "test@naver.com", 
+//     motto : "하리보333",
+//     profil_path : "/Users/yong/Desktop/MERGE/팀원미모티콘/지용님.png"
+// };
+
+
+// test.updateMember(memberNumber, updatedData).then(function(data) {
+//     console.log(data);
+// });
